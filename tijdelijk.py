6088cf275a49790e65be23f20e6fa84d82ea580d
verbakel1000 <verbakel1000@gmail.com>
2025-09-15 22:07:34 +0200

    Add files via upload

---
+++ tijdelijk.py
@@ -0,0 +1,17 @@
+#Huiswerk 7.15
+prijzen = { 
+    "aardbei": 3,
+    "vanille":  4,
+    "chocolade": 5
+}
+aanbieding = prijzen["aardbei"] *0.8
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else :
+        print(el.lower())
+   
